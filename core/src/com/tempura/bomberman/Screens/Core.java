ec449fc7f8aa2ea581aeea167fdc26f1a6d20f98
mendez22 <mendez22@192.168.0.110>
2022-11-24 23:56:46 +0800

    "added core and splashworker"

---
+++ core/src/com/tempura/bomberman/Screens/Core.java
@@ -0,0 +1,46 @@
+package com.tempura.bomberman.Screens;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Core extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	Stage stage;
+	private SplashWorker splashWorker;
+	
+	@Override
+	public void create() {
+		splashWorker.closeSplashScreen();
+		batch = new SpriteBatch();
+		img = new Texture("sprites/tempuragames.png");
+	}
+
+	@Override
+	public void render() {
+		super.render();
+		ScreenUtils.clear(Color.BLACK);
+		batch.begin();
+		batch.draw(img,67,-15);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose() {
+		batch.dispose();
+		img.dispose();
+	}
+	
+	public SplashWorker getSplashWorker() {
+		return splashWorker;
+	}
+	
+	public void setSplashWorker(SplashWorker splashWorker) {
+		this.splashWorker = splashWorker;
+	}
+}
